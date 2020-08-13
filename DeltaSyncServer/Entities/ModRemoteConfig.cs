3806ffebf59b943b1dee96f70b087ce3fbdcff5d
RomanPort <34401219+Roman-Port@users.noreply.github.com>
2020-08-13 14:50:05 -0500

    Upgraded config

--- DeltaSyncServer/Entities/ModRemoteConfig.cs
+++ DeltaSyncServer/Entities/ModRemoteConfig.cs
@@ -7,14 +7,14 @@ namespace DeltaSyncServer.Entities
     public class ModRemoteConfig
     {
         public int modules_dinos_chunksize = 20;
-        public float modules_dinos_tickrate = 1f;
-        public float modules_dinos_cyclebreak = 10;
+        public float modules_dinos_tickrate = 5f;
+        public float modules_dinos_cyclebreak = 60;
 
-        public int modules_inventories_chunksize = 40;
-        public float modules_inventories_tickrate = 1f;
-        public float modules_inventories_cyclebreak = 10;
+        public int modules_inventories_chunksize = 50;
+        public float modules_inventories_tickrate = 5f;
+        public float modules_inventories_cyclebreak = 60;
 
-        public float modules_livedinos_tickrate = 1.25f;
+        public float modules_livedinos_tickrate = 3f;
         public float modules_livedinos_maxchunk = 40;
         public float modules_livedinos_maxchecked = 400;
         public float modules_livedinos_refreshtime = 30;
@@ -29,6 +29,6 @@ namespace DeltaSyncServer.Entities
             8
         };
 
-        public float modules_realtimeplayers_refreshtime = 5;
+        public float modules_realtimeplayers_refreshtime = 3;
     }
 }
